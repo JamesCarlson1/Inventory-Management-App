004578ef47107293664c021e43865d755209f085
James Carlson <Carlson4607@gmail.com>
2026-09-08 18:19:07 -0500

    Made schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE accounts (
+    account_id INTEGER PRIMARY KEY,
+    account_name TEXT,
+    UNIQUE(account_name)
+);
+
+CREATE TABLE items (
+    item_id INTEGER PRIMARY KEY,
+    account_id INTEGER,
+    item_name TEXT,
+    amt INTEGER,
+    FOREIGN KEY (account_id) REFERENCES accounts(account_id) ON DELETE CASCADE,
+    UNIQUE(account_id, item_name),
+    CHECK (amt >= 0)
+);
