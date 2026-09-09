ba261e644cbb04c6cfb63c6c8421f2c61158e763
James Carlson <Carlson4607@gmail.com>
2026-09-09 10:27:04 -0500

    Added DAO files and DAO folder

---
+++ src/main/java/com/james/inventory/dao/AccountDao.java
@@ -0,0 +1,7 @@
+package com.james.inventory.dao;
+
+// The Interface
+
+public class AccountDao {
+    
+}
---
+++ src/main/java/com/james/inventory/dao/SqliteAccountDao.java
@@ -0,0 +1,7 @@
+package com.james.inventory.dao;
+
+// The JDBC Implementation
+
+public class SqliteAccountDao {
+    
+}
