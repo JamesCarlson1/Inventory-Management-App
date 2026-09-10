4d0f49bd069f7516e36971c206df5c462326db27
James Carlson <Carlson4607@gmail.com>
2026-09-10 09:37:31 -0500

    Made Connection point in SqliteAccountDao

--- src/main/java/com/james/inventory/dao/SqliteAccountDao.java
+++ src/main/java/com/james/inventory/dao/SqliteAccountDao.java
@@ -1,7 +1,40 @@
 package com.james.inventory.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+import com.james.inventory.domain.Account;
+
 // The JDBC Implementation
 
 public class SqliteAccountDao implements AccountDao {
+    // Makes the Database Connection variable.
+    private final Connection connection;
+
+    public SqliteAccountDao (Connection connection) {
+        this.connection = connection;
+    }
     
+    public List<Account> findAll() throws SQLException {
+
+    }
+
+    public Optional<Account> findById (Long accountId) throws SQLException {
+
+    }
+
+    public Optional<Account> findByUsername (String username) throws SQLException {
+
+    }
+
+    public Account create (Account account) throws SQLException {
+
+    }
+
+    public void delete (Long accountId) throws SQLException {
+
+    }
 }
