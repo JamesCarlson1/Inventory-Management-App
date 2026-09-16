65088a1a19bb3944337836a0449c9661eaf5de90
James Carlson <Carlson4607@gmail.com>
2026-09-15 19:29:02 -0500

    Finished most methods in AccountService

--- src/main/java/com/james/inventory/service/AccountService.java
+++ src/main/java/com/james/inventory/service/AccountService.java
@@ -16,18 +16,26 @@ public class AccountService {
     }
 
     public List<Account> findAll() throws SQLException {
-
+        return this.accountDao.findAll();
     }
 
     public Optional<Account> findById (Long accountId) throws SQLException {
-
+        return this.accountDao.findById(accountId);
     }
-    public void delete (Long accountId) throws SQLException {
 
+    public void delete (Long accountId) throws SQLException {
+        this.accountDao.delete(accountId);
     }
 
     public Account createAccount (String username) throws SQLException {
+        Account ifExists = accountDao.findByUsername(username);
 
+        if (ifExists == null) {
+            Account account = new Account(null, username);
+            return accountDao.create(account);
+        } else {
+            throw new IllegalArgumentException("Username already exists: " + username);
+        }
     }
 
     public void updateUsername(Long accountId, String newUsername) throws SQLException {
