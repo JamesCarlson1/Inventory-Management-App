f6675f128bc6a54ecec817e1686243654a778319
James Carlson <Carlson4607@gmail.com>
2026-09-16 12:45:51 -0500

    Fixing createItem

--- src/main/java/com/james/inventory/service/ItemService.java
+++ src/main/java/com/james/inventory/service/ItemService.java
@@ -15,33 +15,31 @@ public class ItemService {
     private final ItemDao itemDao;
     private final AccountDao accountDao;
 
-    public ItemService (ItemDao itemDao, AccountDao acountDao) {
+    public ItemService (ItemDao itemDao, AccountDao accountDao) {
         this.itemDao = itemDao;
         this.accountDao = accountDao;
     }
 
     public List<Item> findByAccountId (Long accountId) throws SQLException {
-        return this.findByAccountId(accountId);
+        return this.itemDao.findByAccountId(accountId);
     }
 
-    public List<Item> findByItemId (Long itemId) throws SQLException {
-        return this.findByItemId(itemId);
+    public Optional<Item> findByItemId (Long itemId) throws SQLException {
+        return this.itemDao.findByItemId(itemId);
     }
 
     public void delete (Long itemId) throws SQLException {
         this.itemDao.delete(itemId);
     }
 
-    public createItem (Long accountId, String itemName, long amt) throws SQLException {
-        Optional<Item> ifExists = accountDao.findById();
-        boolean nameTaken = ifExists.stream().anyMatch(item -> item.getItemName().equals(itemName));
-
-        // Can't do "null" since it can never return null.
-        if (ifExists.isPresent()) {
-            throw new IllegalArgumentException("ERROR: Username already exists: " + username);
+    public Item createItem (Long accountId, String itemName, long amt) throws SQLException {
+        Optional<Account> ifExists = accountDao.findById(accountId);
+        if (!ifExists.isPresent()) {
+            throw new IllegalArgumentException("ERROR: Could not locate account: " + accountId);
         }
-        Account account = new Account(null, username);
-        return accountDao.create(account);
+        List<Item> existingItems = itemDao.findByAccountId(accountId);
+        boolean nameTaken = ifExists.stream().anyMatch(existingItems -> existingItems.getItemName().equals(itemName));
+
     }
 
     public receive (Long itemId, long amt) throws SQLException {
